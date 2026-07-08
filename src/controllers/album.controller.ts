6a178bc4fce4793c9e998a7d1eed6b91edbbd5e3
EDWINCSN <c.edwin32@hotmail.com>
2026-07-07 23:41:00 -0500

    feat: implementar actualizarAlbum con validacion de existencia

--- src/controllers/album.controller.ts
+++ src/controllers/album.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import prisma from "../database/prisma.js";
+import PrismaC
 
 
 //FUNCION PARA CREAR ALBUM
@@ -20,6 +21,33 @@ export const obtenerAlbumes=async(req:Request, res:Response)=>{
     try {
         const albumes= await prisma.album.findMany();
         res.status(200).json(albumes);
+
+    } catch (error) {
+        res.status(500).json({error: "Error al recuperar album "+error})
+    }
+}
+
+
+//FUNCION MODIFICAR UN ALBUN POR ID
+export const actualizarAlbum=async(req:Request, res:Response)=>{
+    try {
+        const {id}=req.params;
+        const {titulo, artista, anio}=req.body;
+
+        const existeAlbum= await prisma.album.findUnique({
+            where:{id:Number(id)}
+        });
+
+        if(!existeAlbum){
+           return res.status(404).json("No existe album para actualizar");
+        }
+
+        const albumActualizado= await prisma.album.update({
+            where:{id:Number(id)},
+            data:{titulo, artista, anio}
+        }); 
+
+        res.status(200).json(albumActualizado);
 
     } catch (error) {
         res.status(500).json({error: "Error al recuperar album "+error})
