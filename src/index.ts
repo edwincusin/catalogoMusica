37e1f6bac5a15bfa11ce4e1d2c10b46182f4b66d
EDWINCSN <c.edwin32@hotmail.com>
2026-07-08 00:45:29 -0500

    feat: registrar router de canciones en index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import albumesRouter from './routes/albumes.js'
+import cancionesRouter from './routes/canciones.js'
 
 const app=express();
 
@@ -7,6 +8,7 @@ const PUERTO=3001;
 
 app.use(express.json());
 app.use("/albumes",albumesRouter)
+app.use("/canciones",cancionesRouter)
 
 app.listen(PUERTO,()=>{
     console.log("servidor corriendo en el puerto : "+PUERTO)
